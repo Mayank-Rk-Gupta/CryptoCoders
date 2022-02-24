3b6ad367aa265e50524b97062fa8187021f608c6
Mayank Gupta <77333297+Mayank-Rk-Gupta@users.noreply.github.com>
2022-02-24 23:30:00 +0530

    Add Build File for hosting

---
+++ client/build/precache-manifest.fb03663e31ea6df4f03d54fc6f79fcfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e79d296a87bd1a82d601446413ef087",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75e8576604c1eaff70ca",
+    "url": "/static/css/2.37c457eb.chunk.css"
+  },
+  {
+    "revision": "d221f6517cafb3a08e04",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "75e8576604c1eaff70ca",
+    "url": "/static/js/2.73742a31.chunk.js"
+  },
+  {
+    "revision": "d221f6517cafb3a08e04",
+    "url": "/static/js/main.fc8a51ff.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
